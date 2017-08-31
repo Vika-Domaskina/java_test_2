67113300d1001189443b896e9a0319263dda9ae4
Victoria <v1ktor1ya.domask1na@gmail.com>
2017-08-31 17:05:42 +0300

    step 1

--- src/BigGame/Window1.java
+++ src/BigGame/Window1.java
@@ -1,31 +1,47 @@
 package BigGame;
 
-import java.awt.event.*;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.io.*;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
 /**
  * Created by Viktoriya.D on 7/28/2017.
  */
 public class Window1 {
     private JButton clientButton;
-    private JButton xButton;
+    private JButton serverButton;
     private JTextField chooseYourTypeTextField;
     private JPanel mainPanel;
+    private JTextField IPField;
+    private JButton IPButton;
+    private JFrame frame;
+
+    ServerSocket serverSocket = null;
+    Socket clientSocket = null;
+    Socket remoteClientSocket = null;
+    BufferedWriter fromServerToClient = null;
+    BufferedReader toServerFromClient = null;
+    BufferedWriter fromClientToServer = null;
+    BufferedReader toClientFromServer = null;
+
+    Battlefield bg = null;
 
     public Window1() {
-        clientButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                chooseYourTypeTextField.setText("Selected!");
-                clientButton.setVisible(false);
-                xButton.setVisible(false);
-                /*mainPanel.setVisible(false);*/
-            }
-        });
+        serverButton = new JButton("I want to be a server!");
+        IPButton = new JButton("Connect!");
+        clientButton = new JButton("I want to be a client!");
+        IPField.setColumns(24);
+        IPButton = new JButton("Connect!");
 
-        xButton.addActionListener(new ActionListener() {
+        clientButton.addActionListener(new ActionListener() {
             /**
              * Invoked when an action occurs.
              *
@@ -34,9 +50,90 @@ public class Window1 {
             @Override
             public void actionPerformed(ActionEvent e) {
                 chooseYourTypeTextField.setText("Selected!");
+                IPField.setEnabled(true);
+                IPButton.setEnabled(true);
                 clientButton.setVisible(false);
-                xButton.setVisible(false);
+                serverButton.setVisible(false);
+            }
+        });
+
+        serverButton.addActionListener(new ActionListener() {
+            /**
+             * Invoked when an action occurs.
+             *
+             * @param e
+             */
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                chooseYourTypeTextField.setText("Selected!");
+                InetAddress thisIp = null;
+                try {
+                    thisIp = InetAddress.getLocalHost();
+                } catch (UnknownHostException ex) {
+                    JOptionPane.showMessageDialog(null, "Fatal error! Can't get IP!", "Sorry, fatal error!", 1);
+                    System.exit(1);
+                }
+                try {
+                    serverSocket = new ServerSocket(1234);
+                } catch (IOException ioe) {
+                    JOptionPane.showMessageDialog(null, "Error!");
+                    System.exit(1);
+                }
+
+                clientButton.setVisible(false);
+                serverButton.setVisible(false);
                 /*mainPanel.setVisible(false);*/
+            }
+        });
+        IPButton.addActionListener(new ActionListener() {
+            /**
+             * Invoked when an action occurs.
+             *
+             * @param e
+             */
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                InetAddress ia = null;
+
+                try {
+
+                    remoteClientSocket = new Socket(ia = InetAddress.getByName(IPField.getText()), 1234);
+                    fromClientToServer = new BufferedWriter(new OutputStreamWriter(remoteClientSocket.getOutputStream()));
+                    toClientFromServer = new BufferedReader(new InputStreamReader(remoteClientSocket.getInputStream()));
+                    bg = new Battlefield();
+                } catch (UnknownHostException uhe) {
+                    System.err.println("Don't know about host: " + ia.getHostAddress());
+                    System.exit(1);
+                } catch (IOException ioe) {
+                    System.err.println("Couldn't get I/O for the connection to: " + ia.getHostAddress());
+                    System.exit(1);
+                }
+                try {
+
+                    clientSocket = serverSocket.accept();
+                    fromServerToClient = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+                    toServerFromClient = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                } catch (IOException ety) {
+                    JOptionPane.showMessageDialog(null, "Client disconnected...");
+                    System.exit(1);
+                }
+            }
+        });
+        IPField.addMouseListener(new MouseAdapter() {
+            /**
+             * {@inheritDoc}
+             *
+             * @param e
+             */
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                if (IPField.getText().equals("Input server's IP here...")) {
+
+                    // clear the box...
+
+                    IPField.setText("");
+                }
             }
         });
     }

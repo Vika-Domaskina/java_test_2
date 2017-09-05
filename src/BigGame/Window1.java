6f2f383bee620889ce11ee8d025f83f2b914d929
Victoria <v1ktor1ya.domask1na@gmail.com>
2017-09-05 17:02:51 +0300

    server and client in gui

--- src/BigGame/Window1.java
+++ src/BigGame/Window1.java
@@ -7,10 +7,8 @@ import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.io.*;
-import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.net.UnknownHostException;
 
 /**
  * Created by Viktoriya.D on 7/28/2017.
@@ -21,18 +19,12 @@ public class Window1 {
     private JTextField chooseYourTypeTextField;
     private JPanel mainPanel;
     private JTextField IPField;
-    private JButton IPButton;
+    private JButton connectButton;
     private JFrame frame;
 
-    ServerSocket serverSocket = null;
-    Socket clientSocket = null;
-    Socket remoteClientSocket = null;
-    BufferedWriter fromServerToClient = null;
-    BufferedReader toServerFromClient = null;
-    BufferedWriter fromClientToServer = null;
-    BufferedReader toClientFromServer = null;
 
     Window2 bg = null;
+    boolean isServer;
 
     public Window1() {
         frame = new JFrame("My Game");
@@ -51,11 +43,10 @@ public class Window1 {
              */
             @Override
             public void actionPerformed(ActionEvent e) {
-                chooseYourTypeTextField.setText("Selected!");
-                IPField.setEnabled(true);
-                IPButton.setEnabled(true);
-                clientButton.setVisible(false);
-                serverButton.setVisible(false);
+                chooseYourTypeTextField.setText("Selected client!");
+                JOptionPane.showMessageDialog(frame, "Please input in text box IP server for connecting");
+                isServer=false;
+                serverButton.setEnabled(true);
             }
         });
 
@@ -67,27 +58,13 @@ public class Window1 {
              */
             @Override
             public void actionPerformed(ActionEvent e) {
-                serverButton.setText("You choose server!");
-                String thisIp = null;
-                try {
-                    thisIp = InetAddress.getLocalHost().getHostAddress();
-                    JOptionPane.showMessageDialog(null, "My IP is " + thisIp);
-                } catch (UnknownHostException ex) {
-                    JOptionPane.showMessageDialog(null, "Fatal error! Can't get IP!", "Sorry, fatal error!", 1);
-                    System.exit(1);
-                }
-                try {
-                    serverSocket = new ServerSocket(1234);
-                } catch (IOException ioe) {
-                    JOptionPane.showMessageDialog(null, "Error!");
-                    System.exit(1);
-                }
+                chooseYourTypeTextField.setText("Selected server!");
+                clientButton.setEnabled(true);
+                isServer=true;
 
-                clientButton.setVisible(false);
-                serverButton.setVisible(false);
             }
         });
-        IPButton.addActionListener(new ActionListener() {
+        connectButton.addActionListener(new ActionListener() {
             /**
              * Invoked when an action occurs.
              *
@@ -95,28 +72,15 @@ public class Window1 {
              */
             @Override
             public void actionPerformed(ActionEvent e) {
-                InetAddress ia = null;
-                try {
-
-                    remoteClientSocket = new Socket(ia = InetAddress.getByName(IPField.getText()), 1234);
-                    fromClientToServer = new BufferedWriter(new OutputStreamWriter(remoteClientSocket.getOutputStream()));
-                    toClientFromServer = new BufferedReader(new InputStreamReader(remoteClientSocket.getInputStream()));
-                    bg = new Window2();
-                } catch (UnknownHostException uhe) {
-                    System.err.println("Don't know about host: " + ia.getHostAddress());
-                    System.exit(1);
-                } catch (IOException ioe) {
-                    System.err.println("Couldn't get I/O for the connection to: " + ia.getHostAddress());
-                    System.exit(1);
-                }
-
-                try {
-                    clientSocket = serverSocket.accept();
-                    fromServerToClient = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
-                    toServerFromClient = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                } catch (IOException ety) {
-                    JOptionPane.showMessageDialog(null, "Client disconnected...");
-                    System.exit(1);
+                if (!isServer && !IPField.getText().equals("")){
+                        Client.createClientConnection(IPField.getText());
+                    while (true) {
+                        Client.chat();
+                    }
+                } else if(isServer){
+                    Server.createServerConnection();
+                } else {
+                    JOptionPane.showMessageDialog(frame, "Could you please input IP server in text box!!!");
                 }
             }
         });
@@ -129,12 +93,7 @@ public class Window1 {
             @Override
             public void mouseClicked(MouseEvent e) {
                 super.mouseClicked(e);
-                if (IPField.getText().equals("Input server's IP here...")) {
-
-                    // clear the box...
-
-                    IPField.setText("");
-                }
+                serverButton.setEnabled(true);
             }
         });
     }

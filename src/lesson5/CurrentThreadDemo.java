f8d6e3421414f37b1e8ba4305f6b508045218d99
Victoria <v1ktor1ya.domask1na@gmail.com>
2017-09-29 10:47:52 +0300

    pp

--- src/lesson5/CurrentThreadDemo.java
+++ src/lesson5/CurrentThreadDemo.java
@@ -4,4 +4,21 @@ package lesson5;
  * Created by Viktoriya.D on 9/29/2017.
  */
 public class CurrentThreadDemo {
+    public static void main(String[] args) {
+        Thread thread = Thread.currentThread();
+        System.out.println("My current thread " + thread);
+        thread.setName("My thread");
+        System.out.println(thread);
+        try{
+            for (int i=5;i>0;i--){
+                if(i==3) {
+                    System.out.println(i);
+                    thread.interrupt();
+                    Thread.sleep(2000);
+                }
+            }
+        }catch (InterruptedException e){
+            System.out.println("main execution thread interrupted");
+        }
+    }
 }
